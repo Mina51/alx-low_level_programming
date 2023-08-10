f11b578fc6af4d09921c7d5bf1a6e54d84dc8b19
Mina51 <lahouijamina51@gmail.com>
2023-08-10 16:28:10 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - main function
- *
- * Return: 1 when successfull
- */
+* main - A c program that prints a line to the standard error
+*
+* Return: 1 Success
+*/
 int main(void)
 {
-        fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-        return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
