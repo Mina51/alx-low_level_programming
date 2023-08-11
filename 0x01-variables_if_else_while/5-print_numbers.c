72769a9ca2e6c6e82974b202c1c6c079f97772e9
Mina51 <lahouijamina51@gmail.com>
2023-08-11 11:13:45 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Progr Entry point
+ * Return: 0 when success
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
